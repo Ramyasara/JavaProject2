88a536ca0642a1586a79daa363c44a48c730766a
Ramyasara <ramyabanu1815@gmail.com>
2020-02-03 06:59:06 +0530

    Java Project 2 added

---
+++ src/Employee/Employee1.java
@@ -0,0 +1,16 @@
+package Employee;
+
+public class Employee1 {
+	private void empid() {
+	System.out.println("Employee id is 123");
+	}
+	private void empname() {
+		System.out.println("Employee Name is Xavier");
+	}
+	public static void main(String[] args) {
+		Employee1 e = new Employee1();
+		e.empid();
+		e.empname();				
+	}
+
+}
